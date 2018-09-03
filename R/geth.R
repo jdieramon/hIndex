64165adc2d06d5b26e2e5fb4a972d4f1b801a4c3
jdieramon <josedie@Joses-MacBook.local>
2018-09-03 17:43:58 +0200

    update functions

--- R/geth.R
+++ R/geth.R
@@ -15,7 +15,7 @@ clean <- function(fileName) {
     dat=dat[,-8]
     dat = dat[-c(1:6),]
     dat
-    }
+}
 
 
 #' Get the h-index From the Scopus Database
@@ -25,7 +25,7 @@ clean <- function(fileName) {
 #' most recent year [h=x]).
 #'
 #' @param
-#' file a tidy data frame with papers in row and other info in colums
+#' file a tidy data frame with papers in rows and other info in colums
 #' @param
 #' h an integer number with the h-index for the starting year.
 #' @author Jose V. Die
@@ -35,25 +35,29 @@ clean <- function(fileName) {
 #' @seealso \code{rowSums}
 #' @export
 
-geth <- function(file,h=0) {
-    h <- h
-    vals <- numeric(1)
-    cont <- 1
-    for(i in 9:ncol(file)) {
-        ind <- which(rowSums(file[,8:i]) > h)
-        potential.h <- length(rowSums(file[ind,8:i]))
-        if(potential.h > h) {
-            for(j in potential.h:h) {
-                if((sum(rowSums(file[ind,8:i]) >=j) >=j)==TRUE) {
-                    h=j
-                    break                                       }
-            }
-        }
-        cont <- cont + 1
-        vals[cont] <- h
-    }
-    vals
+geth <- function(file, h=0, stop) {
+
+          stop = which(names(file) == as.character(stop))
+
+          h <- h
+          vals <- numeric(1)
+          cont <- 1
+          for(i in 9:stop) {
+                    ind <- which(rowSums(file[,8:i]) > h)
+                    potential.h <- length(rowSums(file[ind,8:i]))
+                    if(potential.h > h) {
+                              for(j in potential.h:h) {
+                                        if((sum(rowSums(file[ind,8:i]) >=j) >=j)==TRUE) {
+                                                  h=j
+                                                  break                                       }
+                              }
+                    }
+                    cont <- cont + 1
+                    vals[cont] <- h
+          }
+          vals
 }
+
 
 
 #' Plot the h-trajectory
@@ -67,17 +71,33 @@ geth <- function(file,h=0) {
 #' @param h the h-index that corresponds to the starting year (by default=0).
 #' @author Jose V. Die
 #' @export
+#' @import ggplot2
+#' @import dplyr
 
 h.plot <- function(file,a,b,h) {
-    if (b > as.numeric(colnames(file)[ncol(file)])){
-        b = as.numeric(colnames(file)[ncol(file)])
-    } # this is the max year that can be plotted
 
-    barplot(geth(file,h), ylab="h-index", names.arg=a:b,
-            col="steelblue")
+          fin = as.numeric(names(dat)[ncol(dat)])
+          stop = ifelse(b > fin, fin, b)
+
+          y = c(a:stop)
+
+          hvals = geth(file, h, stop)
+          hvals
+          print(hvals)
+          print(y)
+          myh = data.frame(Year = y, H = hvals)
+
+          myh %>%
+                    ggplot(aes(x = Year, y = H)) +
+                    geom_point(color = "steelblue", alpha = .7) +
+                    geom_line(color = "steelblue") +
+                    scale_x_discrete(limits = a:stop) +
+                    ylab('h Index')
+
+
 }
 
-#' Format the file and estimate the time that takes to get 1 citation
+#' Format the file and estimate the time (in months) that takes to get 1 citation
 #'
 #' @param file a tidy data frame
 #' @return a tidy data frame that will be used by \code{plot1cite}
@@ -85,20 +105,23 @@ h.plot <- function(file,a,b,h) {
 #' @export
 #' @import dplyr
 
-format1cite <- function(file) {
-    tmp <-  as.Date(Sys.Date(), '%Y/%m/%d')
-    now = as.numeric(format(tmp, '%Y'))
-    time = now - as.numeric(as.character(file[,1]))
-    dat = mutate(file, Cites = apply(file[,8:ncol(file)], 1, sum)) %>%
-        arrange(desc(Cites)) %>%
-        mutate(Years= now - as.numeric(as.character(`Publication Year`))) %>%
-        filter(Years>0) %>%
-        mutate(sC=round(Years*12/Cites,2)) %>%
-        arrange(sC) %>%
-        filter(sC < 12) %>%
-        select(`Publication Year`, `Journal Title`, `sC`)
-    dat
-    }
+get1cite <- function(file) {
+          tmp <-  as.Date(Sys.Date(), '%Y/%m/%d')
+          now = as.numeric(format(tmp, '%Y'))
+          time = now - as.numeric(as.character(file[,1]))
+
+          dat = mutate(file, Cites = apply(file[,8:ncol(file)], 1, sum)) %>%
+                    arrange(desc(Cites)) %>%
+                    mutate(Years= now - as.numeric(as.character(`Publication Year`))) %>%
+                    filter(Years>0) %>%
+                    mutate(avgMonth=round(Years*12/Cites,2)) %>%
+                    arrange(avgMonth) %>%
+                    filter(avgMonth < 12) %>%
+                    rename('Year' = `Publication Year`, 'Journal' = `Journal Title`) %>%
+                    select(Year, Journal, avgMonth)
+          dat
+}
+
 
 #' Plot the time that takes to get 1 citation for a set of publications
 #'
@@ -107,18 +130,17 @@ format1cite <- function(file) {
 #'
 #' @param file a tidy data frame produced by \code{format1cite}
 #' @author Jose V. Die
-#' @export
 #' @importFrom RColorBrewer brewer.pal
 
 plot1cite <- function(file){
-    dat <- format1cite(file)
-    ncolor = sum(as.vector(table(cut(dat$sC, breaks=c(0:12))))>0)
-    cols <- rev(brewer.pal(ncolor, "Blues")) #"BuGn"
-    ntone = as.vector(table(cut(dat$sC, breaks=c(0:12))))[as.vector(table(cut(dat$sC, breaks=c(0:12))))>0]
-    tones=rep(cols, ntone)
-    names=dat$`Publication Year`
-    xx <- barplot(dat$sC, col=tones, ylab = "Months",
-                  main = "Getting 1 citation", xlab="Publication Id.",
-                  ylim=c(1,12), cex.names = 0.8, las=1)
-    text(x = xx, y = dat$sC, label = names, pos = 3, cex = 0.8, col = "steelblue")
+          dat <- format1cite(file)
+          ncolor = sum(as.vector(table(cut(dat$sC, breaks=c(0:12))))>0)
+          cols <- rev(brewer.pal(ncolor, "Blues")) #"BuGn"
+          ntone = as.vector(table(cut(dat$sC, breaks=c(0:12))))[as.vector(table(cut(dat$sC, breaks=c(0:12))))>0]
+          tones=rep(cols, ntone)
+          names=dat$`Publication Year`
+          xx <- barplot(dat$sC, col=tones, ylab = "Months",
+                        main = "Getting 1 citation", xlab="Publication Id.",
+                        ylim=c(1,12), cex.names = 0.8, las=1)
+          text(x = xx, y = dat$sC, label = names, pos = 3, cex = 0.8, col = "steelblue")
 }
